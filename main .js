f862e1ab6935872d582ac13e6b6da2b2ba1529e3
sozani <sys612603@gmail.com>
2023-01-12 22:11:12 +0200

    wo-

--- main .js
+++ main .js
@@ -85,36 +85,36 @@
 // console.log(reverseString(1337));
 // console.log(reverseString("stackoverflow"));
 // console.log(reverseString([1, 2, 3, 4]));
-
-class stack {
-  constructor() {
-    this.items = [];
-  }
-  push(element) {
-    this.items.push(element);
-  }
-  pop() {
-    return this.items.pop();
-  }
-  peek() {
-    return this.items[this.items.length - 1];
-  }
-  isEmpty() {
-    return this.items.length === 0;
-  }
-  size() {
-    return this.items.length;
-  }
-  print() {
-    console.log(this.items.toString());
-  }
-}
-const stack = new stack();
-console.log(stack.isEmpty());
-stack.push(20);
-stack.push(10);
-stack.push(30);
-console.log(stack.size());
-console.print();
-console.log(stack.pop());
-console.log(stack.peek());
+// The way to apply stack:
+// class stack {
+//   constructor() {
+//     this.items = [];
+//   }
+//   push(element) {
+//     this.items.push(element);
+//   }
+//   pop() {
+//     return this.items.pop();
+//   }
+//   peek() {
+//     return this.items[this.items.length - 1];
+//   }
+//   isEmpty() {
+//     return this.items.length === 0;
+//   }
+//   size() {
+//     return this.items.length;
+//   }
+//   print() {
+//     console.log(this.items.toString());
+//   }
+// }
+// const stack = new stack();
+// console.log(stack.isEmpty());
+// stack.push(20);
+// stack.push(10);
+// stack.push(30);
+// console.log(stack.size());
+// console.print();
+// console.log(stack.pop());
+// console.log(stack.peek());
