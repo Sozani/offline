a48394b0ab7030071a60f503c5548644bdd4d6e8
sozani <sys612603@gmail.com>
2023-01-12 21:52:36 +0200

    t

--- main .js
+++ main .js
@@ -96,4 +96,13 @@ class stack {
   pop() {
     return this.items.pop();
   }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
 }
